814bb5559944806be5ce484302e2c154f392e6e2
Kyuu <117708164+kyuung09@users.noreply.github.com>
2022-12-24 00:39:26 +0900

    Revert "Add entity/repository files upload"

--- dev-sokdak/src/main/java/com/example/devsokdak/board/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.devsokdak.board.repository;
-
-import com.example.devsokdak.board.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- dev-sokdak/src/main/java/com/example/devsokdak/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.devsokdak.user.repository;
-
-import com.example.devsokdak.user.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
