f2fd4e0384a041e759186351bab9424718cd2195
kyuung09 <kyuung09@gmail.com>
2022-12-24 14:24:18 +0900

    Add Comment files Upload

---
+++ dev-sokdak/src/main/java/com/example/devsokdak/comment/dto/CommentResponseDto.java
@@ -0,0 +1,27 @@
+package com.example.devsokdak.comment.dto;
+
+import com.example.devsokdak.comment.entity.Comment;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+public class CommentResponseDto {
+    private Long commentId;
+    private Long boardId;
+    private String userId;
+    private String comment;
+    private LocalDateTime modifiedat;
+    private LocalDateTime createdat;
+
+    //생성자
+    public CommentResponseDto(Comment comment) {
+        this.boardId = comment.getBoard().getId();
+        this.commentId = comment.getId();
+        this.userId = comment.getUserId();
+        this.comment = comment.getContent();
+        this.modifiedat = comment.getModifiedAt();
+        this.createdat = comment.getModifiedAt();
+    }
+}
