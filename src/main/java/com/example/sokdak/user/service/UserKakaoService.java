2fb83d53fbc316d1f57bc9d9e1f3098a18b7f630
kyuung09 <kyuung09@gmail.com>
2022-12-29 02:20:40 +0900

    FIX : 카카오 로그인 Redirect Url 수정 #1

--- src/main/java/com/example/sokdak/user/service/UserKakaoService.java
+++ src/main/java/com/example/sokdak/user/service/UserKakaoService.java
@@ -64,7 +64,8 @@ public class UserKakaoService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", "821a7b35336a2f24ae07d82bd194c723");
-        body.add("redirect_uri", "http://localhost:8080/api/user/kakao/callback");
+        body.add("redirect_uri", "http://localhost:5173/login/");
+//        body.add("redirect_uri", "http://localhost:8080/api/user/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
